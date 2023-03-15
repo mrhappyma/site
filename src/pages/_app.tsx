383efb0e140d0338bcc0091a8e0021b01c8f542e
Dominic Ruggiero <dominic@userexe.me>
2023-03-15 14:13:52 +0000

    vercel audiences

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 
+import { Analytics } from "@vercel/analytics/react";
+
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
@@ -7,7 +9,12 @@ config.autoAddCss = false;
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 };
 
 export default MyApp;
