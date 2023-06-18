4ac109f3a1be25c45c5a9acaf7bd4d3a481112b4
Dominic Ruggiero <dominic@userexe.me>
2023-06-17 23:53:45 -0400

    add rotating 'title' replaces static 'aka dominic aka user.exe' will add more later (of course)

---
+++ src/utils/titles.ts
@@ -0,0 +1,18 @@
+const titles = [
+  "a.k.a. Dominic, a.k.a. user.exe",
+  "a semi-aquatic egg-laying mammal of action",
+  "overthink literally everything",
+  "a big fan of the Oxford comma",
+  "your friendly neighborhood typescript enthusiast. there's too many of us.",
+  "mostly harmless",
+  "I am Groot",
+  "I'm not a robot",
+  "I have read and agree to the terms and conditions",
+  '"haha nerd"',
+  "easily distracted",
+  "made this little title thing at way too late at night",
+  "somehow still awake",
+  "had a whole bunch of ideas for this but forgot them all as soon as I started writing them down",
+];
+
+export default titles;
