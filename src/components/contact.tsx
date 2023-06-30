253c9090c160798aea3bf4eb9f8de24d35cee69f
Dominic Ruggiero <dominic@userexe.me>
2023-06-29 21:41:55 -0400

    contact updates

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -8,8 +8,21 @@ const contactCardCollection: React.FC<{ lanyard: Data | undefined }> = ({
     <div className="md:grid md:grid-cols-2 md:gap-2">
       <div className="py-2 md:py-0">
         <Card
+          title="Matrix"
+          description="@druge:beeper.com"
+          url="https://matrix.to/#/@druge:beeper.com"
+          color="animated"
+        />
+      </div>
+      <div className="py-2 md:py-0">
+        <Card
           title="Discord"
-          description={`@${lanyard?.discord_user.username ?? "loading"}`}
+          description={`${lanyard?.discord_user.username ?? "loading"}${
+            lanyard?.discord_user.discriminator &&
+            lanyard.discord_user.discriminator != "0"
+              ? "#" + lanyard.discord_user.discriminator
+              : ""
+          }`}
           url={`https://discord.com/users/${lanyard?.discord_user.id ?? "0"}`}
           /* TODO: Add Discord avatar */
           color="pink-purple"
@@ -30,6 +43,14 @@ const contactCardCollection: React.FC<{ lanyard: Data | undefined }> = ({
           url="mailto:dominic@userexe.me"
           /* TODO: Copy to clipboard instead of opening mail client */
           color="cyan-green"
+        />
+      </div>
+      <div className="py-2 md:py-0">
+        <Card
+          title="Phone (text or Signal)"
+          description="+1 (667) 966-2096"
+          url="sms:+16679662096"
+          color="yellow-pink"
         />
       </div>
     </div>
--- src/components/projectCard.tsx
+++ src/components/projectCard.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/restrict-template-expressions */
-
 const Card: React.FC<{
   title?: string;
   description?: string;
@@ -13,7 +11,7 @@ const Card: React.FC<{
       <div
         className={`drac-bg-${
           props.color ?? "black-light"
-        } rounded-md p-5 transition-all hover:shadow-2xl`}
+        } rounded-md p-5 transition-all hover:scale-110 hover:shadow-2xl`}
       >
         <h2 className={`drac-heading drac-heading-xl`}>{props.title}</h2>
         <p className={`drac-text drac-text-base`}>{props.description}</p>
